4d6745b82ce306ae8c0035c162a4db81f92b104a
Maciech <maciek.chodacki@wp.pl>
2024-04-28 15:33:03 +0200

    Backend initial setup 1

---
+++ src/main/java/com/development/app/BasketballService/BasketballServiceApplication.java
@@ -0,0 +1,13 @@
+package com.development.app.BasketballService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/development/app/BasketballService/BasketballServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.development.app.BasketballService;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballServiceApplicationTests {
+
+}
