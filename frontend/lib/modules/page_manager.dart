9f52d6f66215ecfeaaf121c96ab0c0089b2c686f
kraslavchik <Slavak540@gmail.com>
2024-08-06 20:27:56 +0200

    merge conflict resolve

--- frontend/lib/modules/page_manager.dart
+++ frontend/lib/modules/page_manager.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/modules/dashboard/dashboard.dart';
+import 'package:frontend/modules/page1/page1.dart';
 import 'package:frontend/modules/page2/page2.dart';
 import 'package:frontend/modules/chats/chats.dart';
 import 'package:frontend/modules/page3/input_results/current_drill_page.dart';
-import 'package:frontend/modules/page5/page5.dart';
-
-import 'Page1/Page1.dart';
 
 final Map<int, Widget Function()> pageMap = {
   0: () => const Page1(),
