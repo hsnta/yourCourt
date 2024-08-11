496e38a89f3532a81e454421222113f6281d0e00
kraslavchik <Slavak540@gmail.com>
2024-08-12 01:48:49 +0200

    docker images - done. 2 more services TODO

--- drill-service/src/main/java/com/basketball/drill_service/DrillServiceApplication.java
+++ drill-service/src/main/java/com/basketball/drill_service/DrillServiceApplication.java
@@ -2,8 +2,6 @@ package com.basketball.drill_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DrillServiceApplication {
